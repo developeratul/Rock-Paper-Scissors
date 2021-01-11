dddf61b2407a90c6522d68926b82e230d565d9c0
Ratul <azammmgol@gmail.com>
2021-01-11 14:52:36 +0600

    message function ready

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const game = (choice) => {
   bot = botChoice()
   console.log(bot)
   var result = decideWinner(human, bot)
-  console.log(result)
+  var message = finalMessage(result)
+  console.log(message)
 }
 const botChoice = () => {
   number = Math.floor(Math.random() * 3)
@@ -12,11 +13,20 @@ const botChoice = () => {
 }
 function decideWinner(humanChoice, botChoice) {
   var rpsData = {
-    Rock: { Paper: 1, Scissors: 0, Rock: 0.5 },
-    Paper: { Paper: 0.5, Scissors: 1, Rock: 0 },
-    Scissors: { Paper: 0, Scissors: 0.5, Rock: 1 },
+    Rock: { Paper: 0, Scissors: 1, Rock: 0.5 },
+    Paper: { Paper: 0.5, Scissors: 0, Rock: 1 },
+    Scissors: { Paper: 1, Scissors: 0.5, Rock: 0 },
   }
   var yourScore = rpsData[humanChoice][botChoice]
   var botScore = rpsData[botChoice][humanChoice]
   return [yourScore, botScore]
+}
+function finalMessage([yourScore, botScore]) {
+  if (yourScore === 0) {
+    return { message: 'You Lost', color: 'red' }
+  } else if (yourScore === 0.5) {
+    return { message: 'You Tied', color: 'Yellow' }
+  } else {
+    return { message: 'You Won', color: 'green' }
+  }
 }
