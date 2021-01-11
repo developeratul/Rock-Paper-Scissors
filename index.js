f862b62ac4d1b984b21b0a651f24facd8cf4ea79
Ratul <azammmgol@gmail.com>
2021-01-11 14:38:11 +0600

    rps result is ready

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const game = (choice) => {
   var human, bot
   human = choice.id
   bot = botChoice()
-  console.log(human, bot)
+  console.log(bot)
+  var result = decideWinner(human, bot)
+  console.log(result)
 }
 const botChoice = () => {
   number = Math.floor(Math.random() * 3)
   return ['Rock', 'Paper', 'Scissors'][number]
+}
+function decideWinner(humanChoice, botChoice) {
+  var rpsData = {
+    Rock: { Paper: 1, Scissors: 0, Rock: 0.5 },
+    Paper: { Paper: 0.5, Scissors: 1, Rock: 0 },
+    Scissors: { Paper: 0, Scissors: 0.5, Rock: 1 },
+  }
+  var yourScore = rpsData[humanChoice][botChoice]
+  var botScore = rpsData[botChoice][humanChoice]
+  return [yourScore, botScore]
 }
